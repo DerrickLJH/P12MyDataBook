de0a66010a48b77005f267a20d4195d372416f76
17010457 <17010457@myrp.edu.sg>
2019-07-23 15:46:38 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p12_mydatabook/AboutUsActivity.java
@@ -0,0 +1,24 @@
+package com.myapplicationdev.android.p12_mydatabook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class AboutUsActivity extends AppCompatActivity {
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        iv = findViewById(R.id.iv);
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(this).load(imageUrl)
+                .placeholder(R.drawable.ajax_loader)
+                .error(R.drawable.error)
+                .into(iv);
+    }
+}
---
+++ app/src/main/java/com/myapplicationdev/android/p12_mydatabook/VaccinationFragment.java
@@ -0,0 +1,88 @@
+package com.myapplicationdev.android.p12_mydatabook;
+
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VaccinationFragment extends Fragment {
+    public static final String USER_PREF = "USER_PREF";
+    Button btnEdit;
+    SharedPreferences sp;
+    TextView tvData;
+
+    public VaccinationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.vaccinationfragment, container, false);
+
+
+        btnEdit = v.findViewById(R.id.btnEdit);
+        tvData = v.findViewById(R.id.tvData);
+        sp = getActivity().getSharedPreferences(USER_PREF, Context.MODE_PRIVATE);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        return v;
+    }
+
+    public void openDialog() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+        View mView = getLayoutInflater().inflate(R.layout.dialog_edit, null);
+        final EditText etInput = mView.findViewById(R.id.etInput);
+        etInput.setText(tvData.getText().toString());
+        mBuilder.setTitle("Edit Anniversary").setView(mView).setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String text = etInput.getText().toString();
+                        tvData.setText(text);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        AlertDialog alertDialog = mBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("vac_data", tvData.getText().toString());
+        editor.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        String data = sp.getString("vac_data", "Default Text");
+        tvData.setText(data);
+    }
+}
